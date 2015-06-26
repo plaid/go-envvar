272fd56d9b23f88e6dd0573fd674f2aa1046c123
Alex Browne <stephenalexbrowne@gmail.com>
2015-06-26 14:43:24 -0700

    Add support for default values and custom names via struct tags.

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # go-envvar
-A go library for managing environment variables. Maps to typed values, supports required and optional vars with defaults. 
+A go library for managing environment variables. It maps environment variables to
+typed fields in a struct, and supports required and optional vars with defaults.
+
+## Example Usage
+
+```go
+package main
+
+import (
+	"log"
+	"github.com/plaid/go-envvar/envvar"
+)
+
+type serverEnvVars struct {
+	// Since we didn't provide a default, the environment variable GO_PORT is
+	// required. Parse will set the Port field to the value of the environment
+	// variable and return an error if the environment variable is not set.
+	Port int `envvar:"GO_PORT"`
+	// Since MaxConns has a default value, it is optional. The value of
+	// the environment variable, if set, overrides the default value.
+	MaxConns uint `envvar:"MAX_CONNECTIONS" default:"100"`
+	// Similar to GO_PORT, HOST_NAME is required.
+	HostName string `envvar:"HOST_NAME"`
+}
+
+func main() {
+	vars := serverEnvVars{}
+	if err := envvar.Parse(&vars); err != nil {
+		log.Fatal(err)
+	}
+	// Do something with the parsed environment variables...
+}
+```
--- envvar/envvar.go
+++ envvar/envvar.go
@@ -1,5 +1,6 @@
-// package envvar helps you manage environment variables. It maps environment variables
-// to typed fields in a struct, and supports required and optional vars with defaults.
+// package envvar helps you manage environment variables. It maps environment
+// variables to typed fields in a struct, and supports required and optional
+// vars with defaults.
 package envvar
 
 import (
@@ -9,10 +10,24 @@ import (
 	"syscall"
 )
 
-// Parse parses environment variables into v, which must be a pointer to a struct.
-// For each field in v, Parse will get the environment variable with the same name
-// as the field and set the field to the value of that environment variable, converting
-// it to the appropriate type if needed.
+// Parse parses environment variables into v, which must be a pointer to a
+// struct. Because of the way envvar uses reflection, the fields in v must be
+// exported, i.e. start with a capital letter. For each field in v, Parse will
+// get the environment variable with the same name as the field and set the
+// field to the value of that environment variable, converting it to the
+// appropriate type if needed.
+//
+// Parse supports two struct tags, which can be used together or separately. The
+// struct tag `envvar` can be used to specify the name of the environment
+// variable that corresponds to a field. If the `envvar` struct tag is not
+// provided, the default is to look for an environment variable with the same
+// name as the field. The struct tag `default` can be used to set the default
+// value for a field. The default value must be a string, but will be converted
+// to match the type of the field as needed. If the `default` struct tag is not
+// provided, the corresponding environment variable is required, and Parse will
+// return an error if it is not defined. When the `default` struct tag is
+// provided, the environment variable is considered optional, and if set, the
+// value of the environment variable will override the default value.
 func Parse(v interface{}) error {
 	typ := reflect.TypeOf(v)
 	if typ.Kind() != reflect.Ptr || typ.Elem().Kind() != reflect.Struct {
@@ -26,11 +41,20 @@ func Parse(v interface{}) error {
 	structVal := val.Elem()
 	for i := 0; i < structType.NumField(); i++ {
 		field := structType.Field(i)
-		vString, found := syscall.Getenv(field.Name)
-		if !found {
-			return fmt.Errorf("Missing required environment variable: %s", field.Name)
+		varName := field.Name
+		customName := field.Tag.Get("envvar")
+		if customName != "" {
+			varName = customName
 		}
-		if err := setFieldVal(structVal.Field(i), field.Name, vString); err != nil {
+		defaultVal := field.Tag.Get("default")
+		varVal, found := syscall.Getenv(varName)
+		if !found && defaultVal == "" {
+			return fmt.Errorf("Missing required environment variable: %s", varName)
+		}
+		if defaultVal != "" && varVal == "" {
+			varVal = defaultVal
+		}
+		if err := setFieldVal(structVal.Field(i), varName, varVal); err != nil {
 			return err
 		}
 	}
